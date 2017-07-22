8c126ecc593dd4fcdcf9d6f75f363fd3942499d4
ChanneRen <quixote_ren@163.com>
2017-07-22 18:18:49 +0800

    Update 1#TwoSum.cpp

--- 1#TwoSum.cpp
+++ 1#TwoSum.cpp
@@ -2,8 +2,8 @@
 	problem:	Two Sum
 	difficulty:	easy
 	description:	Given an array of integers, return indices of the two numbers such that they add up to a specific target.
-    		You may assume that each input would have exactly one solution, and you may not use the same element twice.
-  	tags:	Array, Hash Table
+    			You may assume that each input would have exactly one solution, and you may not use the same element twice.
+  	tags:		Array, Hash Table
 	
 	solution:	For this question, we need to return indices, so we could not use two pointers to search after sorting array. 
 			As we have and only have exact two numbers added up to the target number. So,
