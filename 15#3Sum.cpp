34972f0c5b7e814c920e76b4e1a16c08399afef3
ChanneRen <quixote_ren@163.com>
2017-07-22 20:17:07 +0800

    Update 15#3Sum.cpp

--- 15#3Sum.cpp
+++ 15#3Sum.cpp
@@ -1,5 +1,5 @@
 /*
-	problem:	Two Sum
+	problem:	3 Sum
 	difficulty:	medium
 	description:	Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0? 
 			Find all unique triplets in the array which gives the sum of zero.
