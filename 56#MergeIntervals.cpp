c856466e3359fcb59a8753b636b24bd97cbb721f
ChanneRen <quixote_ren@163.com>
2017-09-30 20:53:22 +0800

    Update 56#MergeIntervals.cpp

--- 56#MergeIntervals.cpp
+++ 56#MergeIntervals.cpp
@@ -1,6 +1,6 @@
 
 /*
-	problem:	Jump Game
+	problem:	Merge Intervals
 	difficulty:	medium
 	description:	Given a collection of intervals, merge all overlapping intervals.
 			For example: Given [1,3],[2,6],[8,10],[15,18],
